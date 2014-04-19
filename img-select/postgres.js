cc5b44daf66827d72ce329c9604f644fdce0d5fe
Eva Z <angelacoconuts@Angela-Wonder.(none)>
2014-04-19 09:14:21 +0800

    remove extenal dependencies

--- img-select/postgres.js
+++ img-select/postgres.js
@@ -9,7 +9,27 @@ exports.getPictures = function (req, res){
     var resPictures = [];
     var queryStr = "select * from pictures where query = " + "'" + urlQuery.topic + "';";
 
+    var result = queryPostgres(queryStr);
+
+    var rows = result.rows;
+    for(var i=0; i< rows.length; i++){
+	var picture = {};
+	picture.src = rows[i].img_src;
+	picture.title = rows[i].img_title;
+	picture.selected = rows[i].selected;
+	resPictures.push(picture);
+    }
+
+    res.writeHead( 200 );
+    res.write( JSON.stringify( { "pictures":resPictures } ) );
+    res.end();
+
+}
+
+function queryPostgres(queryStr) {
+
     pg.connect(conString, function(err, client, done) {
+
 	if(err) {
 	    return console.error('error fetching client from pool', err);
 	}
@@ -18,18 +38,7 @@ exports.getPictures = function (req, res){
 	    if(err) {
 		return console.error('error running query', err);
 	    }
-	    var rows = result.rows;
-	    for(var i=0; i< rows.length; i++){
-		var picture = {};
-		picture.src = rows[i].img_src;
-		picture.title = rows[i].img_title;
-		picture.selected = rows[i].selected;
-		resPictures.push(picture);
-	    }
-
-	    res.writeHead( 200 );
-            res.write( JSON.stringify( { "pictures":resPictures } ) );
-            res.end();
+	    return result;
 
 	});
     });
@@ -42,22 +51,11 @@ exports.selectPicture = function (req, res){
     var urlQuery = url.parse(req.url, true).query;
     var queryStr = "update pictures set selected=true where img_src = " + "'" + urlQuery.src + "';";
 
-    pg.connect(conString, function(err, client, done) {
-	if(err) {
-	    return console.error('error fetching client from pool', err);
-	}
-	client.query(queryStr, function(err, result) {
-	    done();
-	    if(err) {
-		return console.error('error running query', err);
-	    }
+    var result = queryPostgres(queryStr);
 
-	    res.writeHead( 200 );
-	    res.write( JSON.stringify( { "success":1 } ) );
-            res.end();
-
-	});
-    });
+    res.writeHead( 200 );
+    res.write( JSON.stringify( { "success":1 } ) );
+    res.end();
 
 }
 
@@ -66,22 +64,11 @@ exports.deSelectPicture = function (req, res){
     var urlQuery = url.parse(req.url, true).query;
     var queryStr = "update pictures set selected=false where img_src = " + "'" + urlQuery.src + "';";
 
-    pg.connect(conString, function(err, client, done) {
-	if(err) {
-	    return console.error('error fetching client from pool', err);
-	}
-	client.query(queryStr, function(err, result) {
-	    done();
-	    if(err) {
-		return console.error('error running query', err);
-	    }
+    var result = queryPostgres(queryStr);
 
-	    res.writeHead( 200 );
-	    res.write( JSON.stringify( { "success":1 } ) );
-            res.end();
-
-	});
-    });
+    res.writeHead( 200 );
+    res.write( JSON.stringify( { "success":1 } ) );
+    res.end();
 
 }
 
@@ -89,27 +76,17 @@ exports.getTopics = function (req, res){
     
     var resTopics = [];
     var queryStr = "select distinct query from pictures;";
-    
-    pg.connect(conString, function(err, client, done) {
-	if(err) {
-	    return console.error('error fetching client from pool', err);
-	}
-	client.query(queryStr, function(err, result) {
-	    done();
-	    if(err) {
-		return console.error('error running query', err);
-	    }
-	    var rows = result.rows;
-	    for(var i=0; i< rows.length; i++){
-		resTopics.push(rows[i].query);
-	    }
 
-	    res.writeHead( 200 );
-            res.write( JSON.stringify( { "topics":resTopics } ) );
-            res.end();
+    var result = queryPostgres(queryStr);
 
-	});
-    });
+    var rows = result.rows;
+    for(var i=0; i< rows.length; i++){
+	resTopics.push(rows[i].query);
+    }
+
+    res.writeHead( 200 );
+    res.write( JSON.stringify( { "topics":resTopics } ) );
+    res.end();
 
 
 }
